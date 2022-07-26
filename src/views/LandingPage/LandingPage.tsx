a275822aa47d2d079b0f2bf5030c1cdf7125fd5e
gtretow <guilherme.tretow@gmail.com>
2022-07-25 22:59:22 -0300

    added: table for cameras already created

--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -4,7 +4,40 @@ import * as S from ".";
 const LandingPage: React.FC = () => {
   return (
     <S.LandingPageWrapper>
-      <S.Container>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</S.Container>
+      <S.CenterDiv>
+        <S.MainHeading>Câmeras Cadastradas</S.MainHeading>
+        <S.Paragraph>
+          Todas suas câmeras cadastradas serão listadas abaixo:
+        </S.Paragraph>
+        <S.Container></S.Container>
+        <S.TableComponent>
+          <S.TableColumn>
+            <S.TableInfo>Nome da câmera</S.TableInfo>
+            <S.TableInfo>Fabricante</S.TableInfo>
+            <S.TableInfo>Número de Série</S.TableInfo>
+            <S.TableInfo></S.TableInfo>
+          </S.TableColumn>
+          <S.TableColumn>
+            <S.TableItem>Camera1</S.TableItem>
+            <S.TableItem>Fabricante1</S.TableItem>
+            <S.TableItem>134134134l1</S.TableItem>
+            <S.TableItem>
+              <button>Excluir</button>
+            </S.TableItem>
+          </S.TableColumn>
+          <S.TableColumn>
+            <S.TableItem>Camera1</S.TableItem>
+            <S.TableItem>Fabricante1</S.TableItem>
+            <S.TableItem>134134134l1</S.TableItem>
+            <S.TableItem>
+              <button>Excluir</button>
+            </S.TableItem>
+          </S.TableColumn>
+        </S.TableComponent>
+        <S.ButtonContainer>
+          <button>Adicionar Câmera</button>
+        </S.ButtonContainer>
+      </S.CenterDiv>
     </S.LandingPageWrapper>
   );
 };
--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const LandingPageWrapper = styled.div`
+export const LandingPageWrapper = styled.main`
   width: 100vw;
   height: 100vh;
   background: ${(props) => props.theme.colors.gray[100]};
@@ -10,10 +10,63 @@ export const LandingPageWrapper = styled.div`
   align-items: center;
 `;
 
-export const Container = styled.div`
+export const CenterDiv = styled.div`
   display: flex;
-
+  flex-direction: column;
+  align-items: center;
   width: 50%;
   background: ${(props) => props.theme.colors.white};
   height: 900px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+
+export const MainHeading = styled.h1`
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+export const TableComponent = styled.table`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  background: red;
+  width: 80%;
+  height: 600px;
+  padding-bottom: 20px;
+`;
+
+export const TableColumn = styled.tr`
+  padding-top: 24px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const TableItem = styled.td`
+  width: 20%;
+
+  color: ${(props) => props.theme.colors.gray[700]};
+  font-size: ${(props) => props.theme.fontSizes.sm};
+`;
+
+export const TableInfo = styled.th`
+  width: 20%;
+  font-size: ${(props) => props.theme.fontSizes.sm};
+`;
+
+export const Paragraph = styled.p`
+  font-size: ${(props) => props.theme.fontSizes.sm};
+`;
+
+export const ButtonContainer = styled.div`
+  margin-top: 20px;
+  background: blue;
+  width: 80%;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
